59dd5f3e8bde98132d224e96e24b35e9b1d3d5ee
William Chong <williamadrian25@hotmail.com>
2021-03-24 17:47:31 -0500

    Buttons and Assets Added on Dashboard

--- screens/dashboardScreen.js
+++ screens/dashboardScreen.js
@@ -1,27 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View,TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Image } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
+const barcodeimg = require('../assets/barcode.png')
+const editimg = require('../assets/edit.png')
 
-class  DashboardScreen extends React.Component{
- doLogout(){
-     AsyncStorage.removeItem("token")
-     .then(
-         res =>{
-            this.props.navigation.navigate('Auth')
-         }
-     )
- }
-    render(){
-        return(
-        <View style={styles.container}>
-        <View style={styles.dashboardWrapper}>
-        <Text style={styles.userText}> hey user</Text>
-        <TouchableOpacity style={styles.logouBtn} onPress={()=>this.doLogout()}>
-         <Text style={styles.logouBtnText}>Logout</Text>   
-        </TouchableOpacity>
-        </View>
-        </View>
+class DashboardScreen extends React.Component {
+    doLogout() {
+        AsyncStorage.removeItem("token")
+            .then(
+                res => {
+                    this.props.navigation.navigate('Auth')
+                }
+            )
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.topuser}>
+
+                    <View>
+                        <Text style={styles.userText}> Welcome user</Text>
+
+                        <TouchableOpacity style={styles.logouBtn} onPress={() => this.doLogout()}>
+                            <Text style={styles.logouBtnText}>Logout</Text>
+                        </TouchableOpacity>
+
+                    </View>
+                </View>
+
+                <View style={{ flexDirection: "row" }}>
+                    <View>
+                        <TouchableOpacity style={styles.dashboardBTn} title="barcode">
+                            <View style={{ justifyContent: "center", alignItems: 'center' }}><Image source={barcodeimg} style={styles.buttonimage} /></View>
+                            <Text style={styles.dashboardBTnText}>Barcode</Text>
+                            <View style={{ justifyContent: "center", alignItems: 'center' }}><View style={styles.borderLine}></View></View>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View>
+                        <TouchableOpacity style={styles.dashboardBTn} title="barcode">
+                            <View style={{ justifyContent: "center", alignItems: 'center' }}><Image source={editimg} style={styles.buttonimage2} /></View>
+                            <Text style={styles.dashboardBTnText}>Registrar</Text>
+                            <View style={{ justifyContent: "center", alignItems: 'center' }}><View style={styles.borderLine}></View></View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+
         )
     }
 }
@@ -29,26 +55,65 @@ class  DashboardScreen extends React.Component{
 export default DashboardScreen;
 
 const styles = StyleSheet.create({
-    container:{
-        height:"100%",
-        alignItems:"center",
-        justifyContent:"center"
+
+    borderLine: {
+        borderWidth: 0.4,
+        borderColor: "#EE712E",
+
+        marginTop: 20,
+        paddingHorizontal: 10,
+        width: "40%",
     },
-    dashboardWrapper:{ },
-    userText:{
-        fontSize:30,
-        fontWeight:"bold",
-        marginBottom:10
+
+    container: {
+        alignItems: "center",
+        justifyContent: "center"
     },
-    logouBtn:{
-        backgroundColor:"red",
-        paddingVertical:10,
-        width:100,
-        alignSelf:"center"
+
+    topuser: {
+        height: "50%",
+        alignItems: "center",
+        justifyContent: "center"
     },
-    logouBtnText:{
-        color:'#fff',
-        textAlign:"center",
-        fontWeight:"bold"
+
+    userText: {
+        textAlign: "center",
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: 10
+    },
+
+    logouBtn: {
+        backgroundColor: "#ee712e",
+        paddingVertical: 10,
+        borderRadius: 20,
+        width: 100,
+        alignSelf: "center"
+    },
+
+    logouBtnText: {
+        color: '#fff',
+        textAlign: "center",
+        fontWeight: "bold"
+    },
+
+    dashboardBTnText: {
+        color: '#ee712e',
+        textAlign: "center",
+        fontWeight: "bold"
+    },
+    buttonimage: {
+        width: 130,
+        height: 120
+    },
+    buttonimage2: {
+        width: 120,
+        height: 120
+    },
+    dashboardBTn: {
+        backgroundColor: "#fff8f5",
+        paddingVertical: 10,
+        width: 150,
+        alignSelf: "center"
     }
 })
