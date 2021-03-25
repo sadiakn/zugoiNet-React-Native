8acc1f20ad393429045c8c55bda605de75dd8274
Abel Serracin <ninjaboy.abel@gmail.com>
2021-03-25 16:03:35 -0500

    Working on Layout issues in Reg Users 1 screen

--- screens/registerUser.js
+++ screens/registerUser.js
@@ -1,19 +1,76 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
 
 class registerUser extends React.Component {
+
+    state = {
+        nombre: "",
+        apellido: "",
+        numero: "",
+        sexo: "",
+        correo: "",
+        password1: "",
+        password2: "",
+
+    }
+    doForm() {
+        const { nombre, apellido, numero, sexo, correo, password1, password2 } = this.state;
+    }
+    onChangeHandle(state, value) {
+        this.setState({
+            [state]: value
+        })
+    }
     render() {
+        const { nombre, apellido, numero, sexo, correo, password1, password2 } = this.state;
         return (
             <View style={styles.container}>
-                <Text> Register User Part1</Text>
+                <View style={styles.formWrapper}>
+                    <Text style={styles.welcomeText}>Crear una Cuenta</Text>
+
+                    <View style={{ justifyContent: "center", alignItems: 'center' }}><View style={styles.borderLine}></View></View>
+
+
+                    <View style={styles.imputWrapper}>
+                        <View style={styles.row}>
+                            <TextInput style={styles.textInputSmall}
+                                placeholder="Nombre"
+                                placeholderTextColor="#333"
+                                value={nombre}
+                                onChangeText={(value) => this.onChangeHandle('nombre', value)}
+                            />
+                            <TextInput style={styles.textInputSmall}
+                                placeholder="Apellido"
+                                placeholderTextColor="#333"
+                                value={apellido}
+                                onChangeText={(value) => this.onChangeHandle('apellido', value)}
+                            />
+                        </View>
+                        <View style={{flex:1}}>
+                            <TextInput style={styles.textInput}
+                                placeholder="Número de Teléfono (Opcional)"
+                                placeholderTextColor="#333"
+                                value={numero}
+                                onChangeText={(value) => this.onChangeHandle('numero', value)}
+                            />
+                        </View>
+                    </View>
+
+
+                </View >
+
                 <TouchableOpacity
-                activeOpacity={0.8}
-                style={styles.btn}
-                onPress={()=>this.props.navigation.navigate('RegU2')}>
-                <Text style={styles.BTnText}>Siguiente</Text>
+                    activeOpacity={0.8}
+                    style={styles.btn}
+                    onPress={() => this.props.navigation.navigate('RegU2')}>
+                    <Text style={styles.BTnText}>Siguiente</Text>
                 </TouchableOpacity>
-            </View>
+
+
+
+            </View >
         )
     }
 }
@@ -21,16 +78,29 @@ class registerUser extends React.Component {
 export default registerUser;
 
 const styles = StyleSheet.create({
+    row: {
+        flex: 1,
+        flexDirection: "row",
+        
+    },
+    col: {
+        
+        flexDirection: "column",
+        
+    },
+    formRow: {
+        marginBottom: 10
+    },
     container: {
         height: "100%",
         alignItems: "center",
         justifyContent: "center"
     },
-    btn:{
-        marginTop:30,
-        paddingVertical: 10 ,
-        borderRadius:20,
-        width:"60%",
+    btn: {
+        marginTop: 30,
+        paddingVertical: 10,
+        borderRadius: 20,
+        width: "60%",
         backgroundColor: "#EE712E",
         alignSelf: "center"
     },
@@ -38,5 +108,68 @@ const styles = StyleSheet.create({
         color: 'white',
         textAlign: "center",
         fontWeight: "bold"
+    },
+    formWrapper: {
+        width: "80%"
+    },
+    imputWrapper: {
+        flexDirection: "column",
+        
+        marginBottom: 20,
+    },
+    textInput: {
+        marginBottom: 15,
+        paddingVertical: 10,
+        backgroundColor: "#FFFFFF",
+        height: 40,
+        paddingHorizontal: 10,
+        color: "#333",
+        borderRadius: 0,
+        borderWidth: 1,
+        borderColor: "black"
+
+    },
+    textInputSmall: {
+        flex: 1,
+        width: "30%",
+        marginBottom: 15,
+        marginHorizontal: 10,
+        paddingVertical: 10,
+        backgroundColor: "#FFFFFF",
+        height: 40,
+        paddingHorizontal: 5,
+        color: "#333",
+        borderRadius: 0,
+        borderWidth: 1,
+        borderColor: "black"
+
+    },
+    welcomeText: {
+        textAlign: 'center',
+        fontSize: 24,
+
+
+    },
+    Btn: {
+        marginTop: 30,
+        paddingVertical: 10,
+        borderRadius: 20,
+        width: "60%"
+
+    },
+    signinText: {
+        textAlign: "center",
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: "bold"
+    },
+    borderLine: {
+        borderWidth: 0.5,
+        borderColor: "#EE712E",
+        marginBottom: 50,
+        marginTop: 20,
+        paddingHorizontal: 10,
+        width: "50%",
+
     }
 })
