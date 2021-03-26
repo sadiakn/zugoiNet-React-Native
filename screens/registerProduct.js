8e183c7b9ff74434ea0adcb312538a968a1d3567
William Chong <williamadrian25@hotmail.com>
2021-03-26 16:53:41 -0500

    RadioButton Test

--- screens/registerProduct.js
+++ screens/registerProduct.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
+import RadioForm, { RadioButton, RadioButtonInput, RadioButtonLabel } from 'react-native-simple-radio-button';
+
+var gender = [
+    { label: 'Hombre', value: 0 },
+    { label: 'Mujer', value: 1 }
+];
 
 class registerProduct extends React.Component {
     render() {
         return (
             <View style={styles.container}>
                 <Text> Register product</Text>
+
+                <RadioForm
+                    radio_props={gender}
+                    initial={-1}
+                    onPress={(value) => {}}
+                    buttonColor={'#ee712e'}
+                    formHorizontal={true}
+                    selectedButtonColor = {'#ee712e'}
+                />
+
             </View>
         )
     }
